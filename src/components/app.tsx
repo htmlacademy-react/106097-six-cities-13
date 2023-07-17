71e1ba0ef3eeaba32a8bef41ddf24b0aa12d92eb
Николай Стебунов <stebunoff@Nikolajs-MacBook-Pro.local>
2023-07-17 17:01:41 +0300

    Add routing

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,4 +1,9 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
 import Homepage from '../pages/homepage';
+import { AppRoute } from '../const';
+import Login from '../pages/login';
+import Favorites from '../pages/favorites';
+import Offer from '../pages/offer';
 
 type AppProps = {
     placesCount: number;
@@ -6,6 +11,15 @@ type AppProps = {
 
 export default function App({placesCount}: AppProps): JSX.Element {
   return (
-    <Homepage placesCount={placesCount}/>
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Root}>
+          <Route index element={<Homepage placesCount={placesCount}/>} />
+          <Route path={AppRoute.Login} element={<Login />} />
+          <Route path={AppRoute.Favorites} element={<Favorites />} />
+          <Route path={AppRoute.Offer} element={<Offer />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,10 @@
 export const Setting = {
   PlacesCount: 10,
 };
+
+export const AppRoute = {
+  Root: '/',
+  Login: 'login',
+  Favorites: 'favorites',
+  Offer: 'offer/:id'
+};
