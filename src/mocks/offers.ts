edc8bda2bd49b4d4aa7c5406d1744cbb051749b6
Николай Стебунов <stebunoff@Nikolajs-MacBook-Pro.local>
2023-07-20 16:33:46 +0300

    Create mocks for offers

---
+++ src/mocks/offers.ts
@@ -0,0 +1,146 @@
+const offers = [
+  {
+    'id': '6af6f711-c28d-4121-82cd-e0b462a27f00',
+    'title': 'Beautiful & luxurious studio at great location',
+    'type': 'apartment',
+    'price': 120,
+    'city': {
+      'name': 'Amsterdam',
+      'location': {
+        'latitude': 52.35514938496378,
+        'longitude': 4.673877537499948,
+        'zoom': 8
+      }
+    },
+    'location': {
+      'latitude': 52.35514938496378,
+      'longitude': 4.673877537499948,
+      'zoom': 8
+    },
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 4,
+    'description': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'bedrooms': 3,
+    'goods': [
+      'Heating'
+    ],
+    'host': {
+      'name': 'Oliver Conner',
+      'avatarUrl': 'https://url-to-image/image.png',
+      'isPro': false
+    },
+    'images': [
+      'https://url-to-image/image.png'
+    ],
+    'maxAdults': 4
+  },
+  {
+    'id': '6af6f711',
+    'title': 'Beautiful flat at great location',
+    'type': 'apartment',
+    'price': 20,
+    'city': {
+      'name': 'Berlin',
+      'location': {
+        'latitude': 45,
+        'longitude': 3,
+        'zoom': 7
+      }
+    },
+    'location': {
+      'latitude': 45,
+      'longitude': 3,
+      'zoom': 7
+    },
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 5,
+    'description': 'Picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'bedrooms': 2,
+    'goods': [
+      'Heating'
+    ],
+    'host': {
+      'name': 'Jason Stathem',
+      'avatarUrl': 'https://url-to-image/image.png',
+      'isPro': false
+    },
+    'images': [
+      'https://url-to-image/image.png'
+    ],
+    'maxAdults': 2
+  },
+  {
+    'id': 'c28d-4121',
+    'title': 'Great apartment',
+    'type': 'apartment',
+    'price': 40,
+    'city': {
+      'name': 'New York',
+      'location': {
+        'latitude': 47,
+        'longitude': 4,
+        'zoom': 8
+      }
+    },
+    'location': {
+      'latitude': 47,
+      'longitude': 4,
+      'zoom': 8
+    },
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 3,
+    'description': 'Behind a a river by the unique lightness of NY.',
+    'bedrooms': 3,
+    'goods': [
+      'Heating'
+    ],
+    'host': {
+      'name': 'Bill Clinton',
+      'avatarUrl': 'https://url-to-image/image.png',
+      'isPro': false
+    },
+    'images': [
+      'https://url-to-image/image.png'
+    ],
+    'maxAdults': 3
+  },
+  {
+    'id': 'e0b462a27f00',
+    'title': 'Kvartira dlya sema',
+    'type': 'apartment',
+    'price': 10,
+    'city': {
+      'name': 'Moscow',
+      'location': {
+        'latitude': 49,
+        'longitude': 5,
+        'zoom': 8
+      }
+    },
+    'location': {
+      'latitude': 49,
+      'longitude': 5,
+      'zoom': 8
+    },
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 3,
+    'description': 'Flat in front of Kremlin.',
+    'bedrooms': 5,
+    'goods': [
+      'Heating'
+    ],
+    'host': {
+      'name': 'Bill Clinton',
+      'avatarUrl': 'https://url-to-image/image.png',
+      'isPro': false
+    },
+    'images': [
+      'https://url-to-image/image.png'
+    ],
+    'maxAdults': 4
+  }
+];
