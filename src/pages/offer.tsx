609aa92dbcc5f0a21e5106ebaeff5dcf5990639b
Николай Стебунов <stebunoff@Nikolajs-MacBook-Pro.local>
2023-08-04 14:42:28 +0300

    feat: add reviews components

--- src/pages/offer.tsx
+++ src/pages/offer.tsx
@@ -6,6 +6,8 @@ import { AppRoute, MAX_RATING } from '../const';
 import { capitalizeFirstLetter } from '../utils';
 import { Header } from '../components/header';
 import { CommentForm } from '../components/comment-form';
+import { ReviewsList } from '../components/reviews-list';
+import { reviews } from '../mocks/reviews';
 
 type OfferProps = {
   offers: Offers;
@@ -114,38 +116,7 @@ export function OfferComponent({offers, favoriteOffers}: OfferProps) {
                 <h2 className="reviews__title">
                   Reviews · <span className="reviews__amount">1</span>
                 </h2>
-                <ul className="reviews__list">
-                  <li className="reviews__item">
-                    <div className="reviews__user user">
-                      <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                        <img
-                          className="reviews__avatar user__avatar"
-                          src="img/avatar-max.jpg"
-                          width="54"
-                          height="54"
-                          alt="Reviews avatar"
-                        />
-                      </div>
-                      <span className="reviews__user-name">Max</span>
-                    </div>
-                    <div className="reviews__info">
-                      <div className="reviews__rating rating">
-                        <div className="reviews__stars rating__stars">
-                          <span style={{ width: '80%' }} />
-                          <span className="visually-hidden">Rating</span>
-                        </div>
-                      </div>
-                      <p className="reviews__text">
-                        A quiet cozy and picturesque that hides behind a a river by
-                        the unique lightness of Amsterdam. The building is green and
-                        from 18th century.
-                      </p>
-                      <time className="reviews__time" dateTime="2019-04-24">
-                        April 2019
-                      </time>
-                    </div>
-                  </li>
-                </ul>
+                <ReviewsList reviews={reviews} />
                 <CommentForm />
               </section>
             </div>
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,28 @@
 export function capitalizeFirstLetter(string: string): string {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+const months = new Map<number, string>([
+  [1, 'January'],
+  [2, 'February'],
+  [3, 'March'],
+  [4, 'April'],
+  [5, 'May'],
+  [6, 'June'],
+  [7, 'July'],
+  [8, 'August'],
+  [9, 'September'],
+  [10, 'October'],
+  [11, 'November'],
+  [12, 'December'],
+]);
+
+export function formatDate(date: string) {
+  const dateObject = new Date(date);
+  const year = dateObject.getFullYear();
+  const month = months.get(dateObject.getMonth());
+  return {
+    'year': year,
+    'month': month
+  };
+}
