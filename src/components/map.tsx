38a5e03921dea06f43c2b69af525fd4a1f13cd84
Николай Стебунов <stebunoff@Nikolajs-MacBook-Pro.local>
2023-08-12 22:21:02 +0300

    refactor: replace pins icons

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -5,8 +5,8 @@ import { useMap } from '../hooks/use-map';
 import { Offers, City, NearbyOffers } from '../types/offer';
 import { useAppSelector } from '../hooks';
 
-const URL_MARKER_DEFAULT = 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
-const URL_MARKER_CURRENT = 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
+const URL_MARKER_DEFAULT = '../../public/img/pin.svg';
+const URL_MARKER_CURRENT = '../../public/img/pin-active.svg';
 
 type MapProps = {
   city: City;
