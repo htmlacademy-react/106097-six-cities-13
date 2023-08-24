f07673fa448057084cb03a89aa76422846a4c721
Николай Стебунов <stebunoff@Nikolajs-MacBook-Pro.local>
2023-08-24 17:14:43 +0300

    feat: add async offers loading action

---
+++ src/store/api-actions.ts
@@ -0,0 +1,18 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { AppDispatch, State } from '../types/state';
+import { AxiosInstance } from 'axios';
+import { APIRoute } from '../const';
+import { Offers } from '../types/offer';
+import { loadOffers } from './action';
+
+export const fetchOfferAction = createAsyncThunk<void, undefined, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'data/fetchOffers',
+  async (_arg, {dispatch, extra: api}) => {
+    const {data} = await api.get<Offers>(APIRoute.Offers);
+    dispatch(loadOffers(data));
+  }
+);
