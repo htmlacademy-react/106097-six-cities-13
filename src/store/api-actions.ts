e8da3e38489e2710bf523079e40b8a7f33ba3abb
Николай Стебунов <stebunoff@Nikolajs-MacBook-Pro.local>
2023-08-31 00:40:52 +0300

    refactor: change api actions names

--- src/store/api-actions.ts
+++ src/store/api-actions.ts
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { Extra } from '../types/state';
-import { APIRoute, AppRoute, AuthorizationStatus } from '../const';
+import { APIRoute, AppRoute, AuthorizationStatus, NameSpace } from '../const';
 import { Offer, Offers } from '../types/offer';
 import { redirectToRoute } from './action';
 import { dropToken, saveToken } from '../services/token';
@@ -10,7 +10,7 @@ import { UserInfo } from '../types/user';
 import { requireAuthorization } from './user/user';
 
 export const fetchOffersAction = createAsyncThunk<Offers, undefined, Extra>(
-  'data/fetchOffers',
+  `${NameSpace.Data}/fetchOffers`,
   async (_arg, {extra: api}) => {
     try {
       const {data} = await api.get<Offers>(APIRoute.Offers);
@@ -22,7 +22,7 @@ export const fetchOffersAction = createAsyncThunk<Offers, undefined, Extra>(
 );
 
 export const fetchOfferAction = createAsyncThunk<Offer, {offerId: Offer['id']}, Extra>(
-  'data/fetchOffer',
+  `${NameSpace.Data}/fetchOffer`,
   async ({offerId}, {extra: api}) => {
     try {
       const {data} = await api.get<Offer>(`${APIRoute.Offers}/${offerId}`);
@@ -34,7 +34,7 @@ export const fetchOfferAction = createAsyncThunk<Offer, {offerId: Offer['id']}, 
 );
 
 export const fetchReviewsAction = createAsyncThunk<Reviews, {offerId: Offer['id']}, Extra>(
-  'data/fetchReviews',
+  `${NameSpace.Data}/fetchReviews`,
   async ({offerId}, {extra: api}) => {
     try {
       const {data} = await api.get<Reviews>(`${APIRoute.Reviews}/${offerId}`);
@@ -46,7 +46,7 @@ export const fetchReviewsAction = createAsyncThunk<Reviews, {offerId: Offer['id'
 );
 
 export const fetchOffersNearbyAction = createAsyncThunk<Offers, {offerId: Offer['id']}, Extra>(
-  'data/fetchOffersNearby',
+  `${NameSpace.Data}/fetchOffersNearby`,
   async ({offerId}, {extra: api}) => {
     try {
       const {data} = await api.get<Offers>(`${APIRoute.Offers}/${offerId}${APIRoute.NearbyOffers}`);
@@ -58,14 +58,14 @@ export const fetchOffersNearbyAction = createAsyncThunk<Offers, {offerId: Offer[
 );
 
 export const checkAuthAction = createAsyncThunk<void, undefined, Extra>(
-  'user/checkAuth',
+  `${NameSpace.User}/checkAuth`,
   async (_arg, {extra: api}) => {
     await api.get(APIRoute.Login);
   }
 );
 
 export const logoutAction = createAsyncThunk<void, undefined, Extra>(
-  'user/logout',
+  `${NameSpace.User}/logout`,
   async (_arg, {dispatch, extra: api}) => {
     await api.delete(APIRoute.Logout);
     dropToken();
@@ -74,7 +74,7 @@ export const logoutAction = createAsyncThunk<void, undefined, Extra>(
 );
 
 export const loginAction = createAsyncThunk<UserInfo, AuthData, Extra>(
-  'user/login',
+  `${NameSpace.User}/login`,
   async ({email, password}, {dispatch, extra: api}) => {
     try {
       const {data} = await api.post<UserInfo>(APIRoute.Login, {email, password});
@@ -90,7 +90,7 @@ export const loginAction = createAsyncThunk<UserInfo, AuthData, Extra>(
 );
 
 export const postReview = createAsyncThunk<Review, {reviewData: ReviewData; offerId: Offer['id']}, Extra>(
-  'data/postReview',
+  `${NameSpace.Data}/postReview`,
   async ({reviewData, offerId}, {extra: api}) => {
     const {data} = await api.post<Review>(`${APIRoute.Reviews}/${offerId}`, reviewData);
     return data;
