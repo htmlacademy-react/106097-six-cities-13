cd529ab60ff1aedf912102886e82a99d28e7701f
Николай Стебунов <stebunoff@Nikolajs-MacBook-Pro.local>
2023-09-04 12:51:17 +0100

    fix: fix sort error

--- src/store/offers-data/offers-data.ts
+++ src/store/offers-data/offers-data.ts
@@ -26,16 +26,16 @@ export const offersData = createSlice({
       state.sortType = action.payload;
       switch (action.payload) {
         case SortTypes.LOW_TO_HIGH:
-          state.offers = [...offers].sort((a, b) => a.price - b.price);
+          state.offers = [...state.offers].sort((a, b) => a.price - b.price);
           break;
         case SortTypes.HIGH_TO_LOW:
-          state.offers = [...offers].sort((a, b) => b.price - a.price);
+          state.offers = [...state.offers].sort((a, b) => b.price - a.price);
           break;
         case SortTypes.TOP_RATED:
-          state.offers = [...offers].sort((a, b) => b.rating - a.rating);
+          state.offers = [...state.offers].sort((a, b) => b.rating - a.rating);
           break;
         default:
-          state.offers = offers;
+          state.offers = [...state.offers];
       }
     },
   },
